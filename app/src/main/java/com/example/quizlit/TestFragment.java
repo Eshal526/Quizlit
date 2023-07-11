26f7ed9d93438569ce3b0def79789ec32ddfaad8
Eshal526 <131014696+Eshal526@users.noreply.github.com>
2023-07-12 01:10:40 +0500

    Main Page

--- app/src/main/java/com/example/quizlit/TestFragment.java
+++ app/src/main/java/com/example/quizlit/TestFragment.java
@@ -40,7 +40,7 @@ public class TestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fargment_test, container, false);
+        return inflater.inflate(R.layout.fragment_test, container, false);
     }
 
     @Override
