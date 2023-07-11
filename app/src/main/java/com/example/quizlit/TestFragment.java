14058d90305ca7cd81ab1a23237fdbe5e2febc13
Eshal526 <131014696+Eshal526@users.noreply.github.com>
2023-07-12 01:05:48 +0500

    Main Page

--- app/src/main/java/com/example/quizlit/TestFragment.java
+++ app/src/main/java/com/example/quizlit/TestFragment.java
@@ -1,64 +1,153 @@
 package com.example.quizlit;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TestFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.Random;
+
 public class TestFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView letterTextView, answerTextView;
+    private char[] skyLetters = {'b', 'd', 'f', 'h', 'k', 'l', 't'};
+    private char[] grassLetters = {'g', 'j', 'p', 'q', 'y'};
+    private char[] rootLetters = {'a', 'c', 'e', 'i', 'm', 'n', 'o', 'r', 's', 'u', 'v', 'w', 'x', 'z'};
+    private String answerString = "";
+    private int questionCounter = 0;
+    private SQLiteDatabase database;
 
     public TestFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TestFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TestFragment newInstance(String param1, String param2) {
-        TestFragment fragment = new TestFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        SQLiteOpenHelper dbHelper = new MyDatabaseHelper(getActivity());
+        database = dbHelper.getWritableDatabase();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        return inflater.inflate(R.layout.fargment_test, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        letterTextView = view.findViewById(R.id.letter_text_view);
+        letterTextView.setText(getRandomLetter());
+
+        answerTextView = view.findViewById(R.id.answer_text_view);
+
+        Button skyButton = view.findViewById(R.id.sky_button);
+        skyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer("Sky Letter");
+            }
+        });
+
+        Button grassButton = view.findViewById(R.id.grass_button);
+        grassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer("Grass Letter");
+            }
+        });
+
+        Button rootButton = view.findViewById(R.id.root_button);
+        rootButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer("Root Letter");
+            }
+        });
+    }
+
+    private void checkAnswer(String expectedAnswer) {
+        if (answerString.equals(expectedAnswer)) {
+            answerTextView.setText("Awesome! Your answer is right");
+        } else {
+            answerTextView.setText("Incorrect! The answer is " + answerString);
+        }
+
+        questionCounter++;
+
+        if (questionCounter == 5) {
+            saveResultsToDatabase();
+            navigateToResultFragment();
+        } else {
+            // Wait for 2 seconds and create a new question
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    letterTextView.setText(getRandomLetter());
+                    answerTextView.setText("");
+                }
+            }, 2000); // 2000 milliseconds = 2 seconds
+        }
+    }
+
+    private void saveResultsToDatabase() {
+        MyDatabaseHelper dbHelper = new MyDatabaseHelper(getActivity());
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        for (int i = 1; i <= questionCounter; i++) {
+            String result = getResultForQuestion(i);
+            MyDatabaseHelper.insertResult(db, i, result);
+        }
+
+        db.close();
+    }
+
+    private void navigateToResultFragment() {
+        ResultFragment resultFragment = new ResultFragment();
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, resultFragment)
+                .commit();
+    }
+
+    private String getRandomLetter() {
+        Random random = new Random();
+        int category = random.nextInt(3);
+        char letter;
+        switch (category) {
+            case 0:
+                letter = skyLetters[random.nextInt(skyLetters.length)];
+                answerString = "Sky Letter";
+                break;
+            case 1:
+                letter = grassLetters[random.nextInt(grassLetters.length)];
+                answerString = "Grass Letter";
+                break;
+            default:
+                letter = rootLetters[random.nextInt(rootLetters.length)];
+                answerString = "Root Letter";
+                break;
+        }
+        return String.valueOf(letter);
+    }
+
+    private String getResultForQuestion(int questionNumber) {
+        String result;
+        if (questionNumber <= questionCounter) {
+            result = "Correct"; // Or retrieve the actual result from your logic
+        } else {
+            result = "Not answered";
+        }
+        return result;
     }
 }
